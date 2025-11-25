58010a5f6be7ef4cf7e329a4b165e1d673786317
Alvaro <aguzman2017@udec.cl>
2025-11-25 19:11:47 -0300

    Generacion patrones

---
+++ run_select_patrones.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+./select_patrones dna patrones-dna 100 10
+./select_patrones dna patrones-dna 100 100
+./select_patrones dna patrones-dna 100 1000
+./select_patrones dna patrones-dna 100 10000
+./select_patrones dna patrones-dna 100 100000
+./select_patrones dna patrones-dna 100 1000000
+./select_patrones dna patrones-dna 10 10000000
+./select_patrones dna patrones-dna 10 100000000
+
--- select_patrones.cpp
+++ select_patrones.cpp
@@ -66,7 +66,7 @@ int main(int argc, char* argv[]) {
     std::cout << "[INFO] Leyendo archivo: " << nombreArchivoEntrada << "..." << std::endl;
     std::string texto = leerArchivo(nombreArchivoEntrada);
     long n = texto.length();
-    std::cout << "[INFO] Leído archivo de " << n << "caracteres." << std::endl;
+    std::cout << "[INFO] Leído archivo de " << n << " caracteres." << std::endl;
 
     if (n < m) {
         std::cerr << "Error: El archivo es mas corto que el largo del patron (m)." << std::endl;
