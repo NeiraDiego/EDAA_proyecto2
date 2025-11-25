7fa9b2881ab1017331be0f2b44deae00d837012e
Diego Neira <diegoneira@gmail.com>
2025-11-25 18:59:57 -0300

    experimentos minimizados

--- experimentos.sh
+++ experimentos.sh
@@ -7,7 +7,7 @@ ARCHIVOS=(
     "sources"
     "dna"
     "proteins"
-    # "genomic.fna"  # Comentado: archivo muy grande (3.2GB), causa std::bad_alloc con SA/SA-LCP
+    "genomic.fna"  # Comentado: archivo muy grande (3.2GB), causa std::bad_alloc con SA/SA-LCP
 )
 
 # Archivos de patrones correspondientes a cada dataset
@@ -15,14 +15,14 @@ PATRONES=(
     "patrones-sources"
     "patrones-dna"
     "patrones-proteins"
-    # "patrones-genomic"  # Comentado: corresponde a genomic.fna
+    "patrones-genomic"  # Comentado: corresponde a genomic.fna
 )
 
 # Programas a ejecutar
 PROGRAMAS=("./FM" "./SA" "./SA-LCP")
 
 # Número de veces que se construye la estructura
-NUM_CONSTRUCCIONES=30
+NUM_CONSTRUCCIONES=1
 
 # Número de veces que se busca cada patrón
 NUM_BUSQUEDAS_POR_PATRON=30
