5081e60fb4e85c557f53cebcf625e6506733e549
Alvaro <aguzman2017@udec.cl>
2025-11-25 19:20:13 -0300

    Generacion patrones

--- run_select_patrones.sh
+++ run_select_patrones.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 
-./select_patrones sources patrones-sources 100 10
-./select_patrones sources patrones-sources 100 100
-./select_patrones sources patrones-sources 100 1000
-./select_patrones sources patrones-sources 100 10000
-./select_patrones sources patrones-sources 100 100000
-./select_patrones sources patrones-sources 100 1000000
-./select_patrones sources patrones-sources 10 10000000
-./select_patrones sources patrones-sources 10 100000000
+./select_patrones proteins patrones-proteins 100 10
+./select_patrones proteins patrones-proteins 100 100
+./select_patrones proteins patrones-proteins 100 1000
+./select_patrones proteins patrones-proteins 100 10000
+./select_patrones proteins patrones-proteins 100 100000
+./select_patrones proteins patrones-proteins 100 1000000
+./select_patrones proteins patrones-proteins 10 10000000
+./select_patrones proteins patrones-proteins 10 100000000
 
