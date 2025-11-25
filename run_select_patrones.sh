f9dc6d1069076b48c94f38294eb0df467585d958
Alvaro <aguzman2017@udec.cl>
2025-11-25 20:37:33 -0300

    Generacion patrones

--- run_select_patrones.sh
+++ run_select_patrones.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 
-./select_patrones GCF_000001405.40_GRCh38.p14_genomic.fna patrones-genomic 100 10
-./select_patrones GCF_000001405.40_GRCh38.p14_genomic.fna patrones-genomic 100 100
+./select_patrones sources patrones-sources 100 10
+#./select_patrones GCF_000001405.40_GRCh38.p14_genomic.fna patrones-genomic 100 10
+#./select_patrones GCF_000001405.40_GRCh38.p14_genomic.fna patrones-genomic 100 100
 #./select_patrones GCF_000001405.40_GRCh38.p14_genomic.fna patrones-genomic 100 1000
 #./select_patrones GCF_000001405.40_GRCh38.p14_genomic.fna patrones-genomic 100 10000
 #./select_patrones GCF_000001405.40_GRCh38.p14_genomic.fna patrones-genomic 100 100000
