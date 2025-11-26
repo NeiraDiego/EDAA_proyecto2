f4ac297cdef07f89be9c6ab750061102e89595dc
Alvaro <aguzman2017@udec.cl>
2025-11-25 22:54:06 -0300

    Generacion patrones

--- run_select_patrones.sh
+++ run_select_patrones.sh
@@ -1,13 +1,44 @@
 #!/bin/bash
 
+./select_patrones dna patrones-dna 100 10
+./select_patrones dna patrones-dna 100 100
+./select_patrones dna patrones-dna 100 1000
+./select_patrones dna patrones-dna 100 10000
+./select_patrones dna patrones-dna 100 100000
+./select_patrones dna patrones-dna 100 1000000
+./select_patrones dna patrones-dna 10 10000000
+./select_patrones dna patrones-dna 10 100000000
+#/select_patrones dna patrones-dna 10 403927746
 
 ./select_patrones sources patrones-sources 100 10
-#./select_patrones GCF_000001405.40_GRCh38.p14_genomic.fna patrones-genomic 100 10
-#./select_patrones GCF_000001405.40_GRCh38.p14_genomic.fna patrones-genomic 100 100
-#./select_patrones GCF_000001405.40_GRCh38.p14_genomic.fna patrones-genomic 100 1000
-#./select_patrones GCF_000001405.40_GRCh38.p14_genomic.fna patrones-genomic 100 10000
-#./select_patrones GCF_000001405.40_GRCh38.p14_genomic.fna patrones-genomic 100 100000
-#./select_patrones GCF_000001405.40_GRCh38.p14_genomic.fna patrones-genomic 100 1000000
-#./select_patrones GCF_000001405.40_GRCh38.p14_genomic.fna patrones-genomic 10 10000000
-#./select_patrones GCF_000001405.40_GRCh38.p14_genomic.fna patrones-genomic 10 100000000
-#./select_patrones GCF_000001405.40_GRCh38.p14_genomic.fna patrones-genomic 10 1000000000
+./select_patrones sources patrones-sources 100 100
+./select_patrones sources patrones-sources 100 1000
+./select_patrones sources patrones-sources 100 10000
+./select_patrones sources patrones-sources 100 100000
+./select_patrones sources patrones-sources 100 1000000
+./select_patrones sources patrones-sources 10 10000000
+./select_patrones sources patrones-sources 10 100000000
+#/select_patrones sources patrones-sources 10 210866607
+
+./select_patrones proteins patrones-proteins 100 10
+./select_patrones proteins patrones-proteins 100 100
+./select_patrones proteins patrones-proteins 100 1000
+./select_patrones proteins patrones-proteins 100 10000
+./select_patrones proteins patrones-proteins 100 100000
+./select_patrones proteins patrones-proteins 100 1000000
+./select_patrones proteins patrones-proteins 10 10000000
+./select_patrones proteins patrones-proteins 10 100000000
+./select_patrones proteins patrones-proteins 10 1000000000
+#/select_patrones proteins patrones-proteins 10 1184051855
+
+
+./select_patrones GCF_000001405.40_GRCh38.p14_genomic.fna patrones-genomic 100 10
+./select_patrones GCF_000001405.40_GRCh38.p14_genomic.fna patrones-genomic 100 100
+./select_patrones GCF_000001405.40_GRCh38.p14_genomic.fna patrones-genomic 100 1000
+./select_patrones GCF_000001405.40_GRCh38.p14_genomic.fna patrones-genomic 100 10000
+./select_patrones GCF_000001405.40_GRCh38.p14_genomic.fna patrones-genomic 100 100000
+./select_patrones GCF_000001405.40_GRCh38.p14_genomic.fna patrones-genomic 100 1000000
+./select_patrones GCF_000001405.40_GRCh38.p14_genomic.fna patrones-genomic 10 10000000
+./select_patrones GCF_000001405.40_GRCh38.p14_genomic.fna patrones-genomic 10 100000000
+./select_patrones GCF_000001405.40_GRCh38.p14_genomic.fna patrones-genomic 10 1000000000
+#/select_patrones GCF_000001405.40_GRCh38.p14_genomic.fna patrones-genomic 10 3339739109
